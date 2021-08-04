f8593b22e2bd70344b9bf2be29d596bc39d2aaa8
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-08-04 22:19:46 +0200

    Initial Commit

---
+++ StaggeredGrid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StaggeredGrid
+//
+//  Created by Michele Manniello on 04/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StaggeredGrid/StaggeredGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaggeredGridApp.swift
+//  StaggeredGrid
+//
+//  Created by Michele Manniello on 04/08/21.
+//
+
+import SwiftUI
+
+@main
+struct StaggeredGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
